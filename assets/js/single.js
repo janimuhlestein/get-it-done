595a39ceed780844b66b706c248d7bd902091dae
Jani Muhlestein <janimuhlestein@gmail.com>
2020-08-31 16:09:43 -0600

    handled more than 30 issues

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,6 +1,7 @@
 //todo add code
 
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -10,6 +11,10 @@ var getRepoIssues = function(repo) {
            response.json().then(function(data) {
                displayIssues(data);
            });
+           //check if api has paginated issues
+           if(response.headers.get("Link")) {
+               displayWarning(repo);
+           }
        }
        else {
            alert("There was a problem with your request!");
@@ -51,6 +56,17 @@ var displayIssues = function(issues) {
      issueEl.appendChild(typeEl);
      issueContainerEl.appendChild(issueEl);
 }
+};
+
+var displayWarning = function(repo) {
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+    //appnd to warning container
+    limitWarningEl.appendChild(linkEl);
+
 };
 
 getRepoIssues("facebook/react");
